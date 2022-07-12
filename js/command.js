cbb4d9d2961671598d2756a107098e5c748c6968
yamakagetoshie <97348551+yamakagetoshie@users.noreply.github.com>
2022-07-12 11:09:44 +0900

    20220712

--- js/command.js
+++ js/command.js
@@ -1,14 +1,14 @@
 function onButtonClick() {
-     var val = document.forms.form1.textBox1.value;
-     var target = document.getElementById("output");
+  var val = document.forms.form1.textBox1.value;
+  var target = document.getElementById("output");
 
-     if (val == "ワイン") {
-         target.innerHTML = "正解です！<br>5秒後にページを移動します。";
-//window.location.href = 'back/main_back.html';
-setTimeout(function(){
-  window.location.href = 'back/main_back.html';
-}, 5*1000);
-     } else {
-         target.innerHTML = "間違っています。<br>再度お試しください。";           
-     }
+  if (val == "ワイン") {
+    target.innerHTML = "正解です！<br>3秒後にページを移動します。";
+    //window.location.href = 'back/main_back.html';
+    setTimeout(function () {
+      window.location.href = 'back/main_back.html';
+    }, 3 * 1000);
+  } else {
+    target.innerHTML = "間違っています。<br>再度お試しください。";
   }
+}
